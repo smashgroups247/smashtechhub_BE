190c36d4ea74beec84e104aecec2eb03c2fc218b
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2026-02-02 15:43:07 +0100

    update: add user model with role field for role-based-access-control

--- src/domain/auth/models/user.model.ts
+++ src/domain/auth/models/user.model.ts
@@ -1,9 +1,11 @@
+// src/domain/auth/models/user.model.ts
 export interface User {
   id: string;
   email: string;
   password: string;
   firstName: string;
   lastName: string;
+  role: 'admin' | 'user'; // Add role field for RBAC
   createdAt?: Date;
   updatedAt?: Date;
   lastLogout?: Date;
